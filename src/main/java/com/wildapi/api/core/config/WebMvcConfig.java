b05b6a17139ab113f96e7c84616649ebc4b702c6
MrWhite <hugoblanc.blend@gmail.com>
2020-04-10 16:46:34 +0200

    fix: cors isssue

--- src/main/java/com/wildapi/api/core/config/WebMvcConfig.java
+++ src/main/java/com/wildapi/api/core/config/WebMvcConfig.java
@@ -1,34 +1,17 @@
 package com.wildapi.api.core.config;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import com.wildapi.api.core.dto.modelmapper.DTOModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.persistence.EntityManager;
-import java.util.List;
-
-// @Configuration
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
-    private final ApplicationContext applicationContext;
-    private final EntityManager entityManager;
-
-    @Autowired
-    public WebMvcConfig(ApplicationContext applicationContext, EntityManager entityManager) {
-        this.applicationContext = applicationContext;
-        this.entityManager = entityManager;
-    }
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-        super.addArgumentResolvers(argumentResolvers);
-        ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().applicationContext(this.applicationContext).build();
-        argumentResolvers.add(new DTOModelMapper(objectMapper, entityManager));
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
