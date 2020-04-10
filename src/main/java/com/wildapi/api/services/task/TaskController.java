402a2c1f709ef68b6045d6798afcfc1f2615263d
MrWhite <hugoblanc.blend@gmail.com>
2020-04-10 14:17:10 +0200

    feat: post task

--- src/main/java/com/wildapi/api/services/task/TaskController.java
+++ src/main/java/com/wildapi/api/services/task/TaskController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/tasks")
@@ -17,4 +20,11 @@ public class TaskController {
     public List<Task> findTasks(){
         return service.getAll();
     }
+
+
+    @PostMapping()
+    public Task postMethodName(@RequestBody Task task) {
+        return service.save(task);
+    }
+    
 }
