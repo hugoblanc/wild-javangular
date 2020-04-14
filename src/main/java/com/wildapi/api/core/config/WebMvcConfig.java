74a7d1fdd48ccf490d169a505488b72902127e2e
MrWhite <hugoblanc.blend@gmail.com>
2020-04-14 11:20:18 +0200

    feat: swagger documentations

--- src/main/java/com/wildapi/api/core/config/WebMvcConfig.java
+++ src/main/java/com/wildapi/api/core/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ package com.wildapi.api.core.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,5 +14,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
+    }
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
