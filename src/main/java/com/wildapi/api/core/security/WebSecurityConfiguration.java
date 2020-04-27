29fac68e7696602e18df8d575eea0d9ecbbcf741
MrWhite <hugoblanc.blend@gmail.com>
2020-04-27 12:46:24 +0200

    feat: fix missing csrf disabled

--- src/main/java/com/wildapi/api/core/security/WebSecurityConfiguration.java
+++ src/main/java/com/wildapi/api/core/security/WebSecurityConfiguration.java
@@ -16,7 +16,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().authorizeRequests()
+        http.cors().and().csrf().disable().authorizeRequests()
                 .anyRequest().permitAll()
 //                .anyRequest().authenticated()
                 .and()
