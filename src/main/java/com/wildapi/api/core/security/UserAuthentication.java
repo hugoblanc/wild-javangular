84b7004550a75f9e507cb69f4c53a8bf118f178f
MrWhite <hugoblanc.blend@gmail.com>
2020-04-28 14:38:10 +0200

    feat: authentication user wrapper

---
+++ src/main/java/com/wildapi/api/core/security/UserAuthentication.java
@@ -0,0 +1,52 @@
+package com.wildapi.api.core.security;
+
+import com.wildapi.api.services.user.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public class UserAuthentication implements Authentication {
+
+    private final User user;
+    private boolean authenticated = true;
+
+    public UserAuthentication(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String getName() {
+        return user.getEmail();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getAuthorities();
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public User getDetails() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return user;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return authenticated;
+    }
+
+    @Override
+    public void setAuthenticated(boolean authenticated) {
+        this.authenticated = authenticated;
+    }
+}
