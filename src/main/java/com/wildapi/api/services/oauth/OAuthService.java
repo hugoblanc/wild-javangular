15e3db14453853bba5ff82181a1df69c051ab8c1
MrWhite <hugoblanc.blend@gmail.com>
2020-04-28 10:24:41 +0200

    variabilise

--- src/main/java/com/wildapi/api/services/oauth/OAuthService.java
+++ src/main/java/com/wildapi/api/services/oauth/OAuthService.java
@@ -7,18 +7,22 @@ import com.wildapi.api.services.oauth.dto.OdysseyUserDto;
 import com.wildapi.api.services.user.User;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OAuthService {
 
-    @Autowired RestService restService;
+    @Autowired
+    RestService restService;
+    @Value("${odyssey.callbackUrl}")
+    private String callbackUrl;
 
     @Autowired
     private ModelMapper modelMapper;
 
-    User handleOAuthCallback(String code){
+    User handleOAuthCallback(String code) {
 
         AskAuthorizationDto askAuth = this.createAskAuthorizationFromCode(code);
 
@@ -26,7 +30,7 @@ public class OAuthService {
 
         AuthResponseDto authResponse = this.restService.postObject("https://odyssey.wildcodeschool.com/oauth/token", askAuth, AuthResponseDto.class);
         System.out.println(authResponse);
-        ResponseEntity<OdysseyUserDto> odysseyUserDtoResponseEntity = this.restService.getObject( "https://odyssey.wildcodeschool.com/api/v2/me", OdysseyUserDto.class,authResponse.getAccess_token());
+        ResponseEntity<OdysseyUserDto> odysseyUserDtoResponseEntity = this.restService.getObject("https://odyssey.wildcodeschool.com/api/v2/me", OdysseyUserDto.class, authResponse.getAccess_token());
         User user = this.modelMapper.map(odysseyUserDtoResponseEntity.getBody(), User.class);
         return user;
     }
@@ -35,7 +39,7 @@ public class OAuthService {
     AskAuthorizationDto createAskAuthorizationFromCode(String code){
 
         AskAuthorizationDto askAuth = new AskAuthorizationDto(
-        "http://localhost:8080/oauth"    
+                callbackUrl
         , "authorization_code",System.getenv("odyssey_secret"), "fc3dfb1962ce7fcdb7b7958e9c6928e78ed030deb8462f7ef9e8db5f47301b5c", code);
         return askAuth;
     }
