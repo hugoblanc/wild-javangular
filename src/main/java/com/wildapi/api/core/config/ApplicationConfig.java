2be0a15cfffc1a08470df892b92675b24dfef728
MrWhite <hugoblanc.blend@gmail.com>
2020-05-02 15:45:11 +0200

    feat: setup scheduling daybook

--- src/main/java/com/wildapi/api/core/config/ApplicationConfig.java
+++ src/main/java/com/wildapi/api/core/config/ApplicationConfig.java
@@ -3,14 +3,17 @@ package com.wildapi.api.core.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @Configuration
+@EnableScheduling
 public class ApplicationConfig {
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
         return modelMapper;
     }
+
 
 }
--- src/main/java/com/wildapi/api/services/daybook/DaybookController.java
+++ src/main/java/com/wildapi/api/services/daybook/DaybookController.java
@@ -1,8 +1,13 @@
 package com.wildapi.api.services.daybook;
 
+import com.wildapi.api.core.security.UserAuthentication;
+import com.wildapi.api.services.user.User;
+import com.wildapi.api.services.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
@@ -14,6 +19,9 @@ public class DaybookController {
 
     @Autowired
     DaybookService service;
+
+    @Autowired
+    UserService userService;
 
     @GetMapping()
     public List<Daybook> getDaybooks(@RequestParam(name = "start", required = false) @DateTimeFormat(pattern = "MMddyyyy") Date startDate, @RequestParam(name = "end", required = false) @DateTimeFormat(pattern = "MMddyyyy") Date endDate) {
@@ -36,5 +44,17 @@ public class DaybookController {
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void deleteDaybook(@PathVariable(value = "id") Long id) {
         service.delete(id);
+    }
+
+
+    @Scheduled(cron = "00 00 02 * * ?")
+    public void scheduleFixedDelayTask() {
+        List<User> users = userService.getAll();
+        for (User u : users) {
+            SecurityContextHolder.getContext().setAuthentication(new UserAuthentication(u));
+            Daybook daybook = service.initEmptyDaybookForUser(u);
+            System.out.println(daybook);
+        }
+
     }
 }
